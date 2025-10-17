856ee4b35680c7c4549ebe6feea65ff8b6f03d07
durogalvan <entradasdurogalvan@gmail.com>
2025-10-17 22:11:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Stock data from the provided table
+const stock = {
+    white: {
+        S: 5,
+        M: 30,
+        L: 41,
+        XL: 21
+    },
+    black: {
+        S: 5,
+        M: 30,
+        L: 41,
+        XL: 21
+    }
+};
+
+// ... (todo el JavaScript que estaba en el HTML) ...
